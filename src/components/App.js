a1d00b0d6197e612834b75c80ab5b6cbd630a042
Beta Anggoro <beta@getlinks.co>
2019-04-10 19:30:02 +0700

    HooksNews

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,70 @@
-import React from 'react'
-import { Global } from '@emotion/core'
-import { AppCss, AppHeaderCss } from 'css/AppCss';
-import {Styles } from 'css/StylesCss'
-import Login from './Login'
-import Register from './Register'
+import React, { useEffect, useState, useRef } from 'react'
+import fetch from 'isomorphic-unfetch'
 
+export default function App() {
+  const [results, setResults] = useState([])
+  const [query, setQuery] = useState('react hooks')
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const searchInputRef = useRef()
+  
 
-const App = ({ login }) => (
-  <>
-  <Global styles={Styles} />
-  <AppCss className='App'>
-    <AppHeaderCss className='App-header'>
-      {!login
-        ? <Login />
-        : <Register />
-      }
-    </AppHeaderCss>
-  </AppCss>
-</>
-)
+  useEffect(() => {
+    getResults()
+  }, [])
 
-export default App;
+  
+  const getResults = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(`http://hn.algolia.com/api/v1/search?query=${query}`)
+      const data = await response.json()
+      setResults(data.hits)
+    } catch (err) {
+      setError(err)
+    }
+    setLoading(false)
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+    getResults()
+  }
+
+  const handleClearSearch = (e) => {
+    setQuery('')
+    searchInputRef.current.focus()
+  }
+  
+  return (
+    <>       
+    <form
+      onSubmit={handleOnSubmit}
+    >
+    
+      <input 
+      type='search'
+      value={query}
+      onChange={e => setQuery(e.target.value)}
+      ref={searchInputRef}
+      />
+      <button type='submit'>Search</button>
+      <button type='button' onClick={handleClearSearch}>Clear</button>
+    </form>
+    { loading ? (
+      <p>Loading results...</p>
+    ) : (    
+    <ul>
+      {console.log(results)}
+      {results && results.map(result => (
+        <li key={result.objectID}>
+          <a href={result.url}>{result.title}</a>
+        </li>
+      ))}
+    </ul>
+    )}
+
+    {error && <p>{error.message}</p>}
+    </>
+  )
+}
