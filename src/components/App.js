be93d4215d9e50badd5d9ff06701b848e4872f5e
Beta Anggoro <beta@getlinks.co>
2019-04-11 16:54:46 +0700

    HackerHooked

--- src/components/App.js
+++ src/components/App.js
@@ -37,34 +37,52 @@ export default function App() {
   }
   
   return (
-    <>       
+    <div className="container max-w md mx-auto p-4 m-2 bg-purple-lightest shadow-lg rounded">
+    <img src="https://icon.now.sh/react/c0c" alt="React" className="right" />
+    <h1 className="text-grey-darkest font-thin">Hooks News</h1>   
     <form
       onSubmit={handleOnSubmit}
+      className="mb-2"
     >
     
       <input 
-      type='search'
-      value={query}
-      onChange={e => setQuery(e.target.value)}
-      ref={searchInputRef}
+        type='search'
+        value={query}
+        onChange={e => setQuery(e.target.value)}
+        ref={searchInputRef}
+        className="border p-1 rounded"
       />
-      <button type='submit'>Search</button>
-      <button type='button' onClick={handleClearSearch}>Clear</button>
+      <button 
+        type='submit'
+        className="bg-orange rounded m-1 p-1"
+      >
+        Search
+      </button>
+      <button
+        type='button'
+        onClick={handleClearSearch}
+        className="bg-teal text-white p-1 rounded"
+      >
+        Clear
+      </button>
     </form>
     { loading ? (
-      <p>Loading results...</p>
+      <p className="font-bold text-orange-dark">Loading results...</p>
     ) : (    
-    <ul>
+    <ul className="list-reset leading-normal">
       {console.log(results)}
       {results && results.map(result => (
-        <li key={result.objectID}>
-          <a href={result.url}>{result.title}</a>
+        <li
+          key={result.objectID}
+          className="text-indigo-dark hover:text-indigo-darkest my-2"
+        >
+          <a href={result.url} className="no-underline py-4">{result.title}</a>
         </li>
       ))}
     </ul>
     )}
 
-    {error && <p>{error.message}</p>}
-    </>
+    {error && <p className="text-red font-bold">{error.message}</p>}
+    </div>
   )
 }
